a68f1135d2e38ac3f7a4533464b49024c20a0751
wanwufusu <760931928@qq.com>
2019-05-16 21:48:02 +0800

    第一次测试

---
+++ src/com/ssm/MainTest.java
@@ -0,0 +1,16 @@
+package com.ssm;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Ruimingz
+ * @create 2019/5/16
+ * @since 1.0.0
+ */
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
