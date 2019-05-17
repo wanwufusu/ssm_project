50e8dec250ea64c55b432538889fb239c9ef8aca
wanwufusu <760931928@qq.com>
2019-05-17 15:44:37 +0800

    杨渝挺

---
+++ Resource/R1543/java/com/ssm/mapper/ProcessMeasureCheckMapper.java
@@ -0,0 +1,30 @@
+package com.ssm.mapper;
+
+import com.ssm.bean.ProcessMeasureCheck;
+import com.ssm.bean.ProcessMeasureCheckExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ProcessMeasureCheckMapper {
+    long countByExample(ProcessMeasureCheckExample example);
+
+    int deleteByExample(ProcessMeasureCheckExample example);
+
+    int deleteByPrimaryKey(String pMeasureCheckId);
+
+    int insert(ProcessMeasureCheck record);
+
+    int insertSelective(ProcessMeasureCheck record);
+
+    List<ProcessMeasureCheck> selectByExample(ProcessMeasureCheckExample example);
+
+    ProcessMeasureCheck selectByPrimaryKey(String pMeasureCheckId);
+
+    int updateByExampleSelective(@Param("record") ProcessMeasureCheck record, @Param("example") ProcessMeasureCheckExample example);
+
+    int updateByExample(@Param("record") ProcessMeasureCheck record, @Param("example") ProcessMeasureCheckExample example);
+
+    int updateByPrimaryKeySelective(ProcessMeasureCheck record);
+
+    int updateByPrimaryKey(ProcessMeasureCheck record);
+}
