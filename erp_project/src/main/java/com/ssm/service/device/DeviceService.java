46fafc6206f1d4b1401a914c9958322197c6109c
YUTIN <827118859@qq.com>
2019-05-18 15:32:42 +0800

    完成了fmc的list

--- erp_project/src/main/java/com/ssm/service/device/DeviceService.java
+++ erp_project/src/main/java/com/ssm/service/device/DeviceService.java
@@ -1,11 +1,10 @@
 package com.ssm.service.device;
 
-import com.ssm.bean.device.DevicePage;
-import com.ssm.bean.device.DeviceType;
+
 
 
 import java.util.List;
 
 public interface DeviceService {
-    List<DeviceType> queryDeviceType(DevicePage devicePage);
+    //List<DeviceType> queryDeviceType(DevicePage devicePage);
 }
