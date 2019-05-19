2f01dd6ea7ac9e1644078edfbda7d94e8698a1f9
yang <mattgoforit.gmail.com>
2019-05-19 14:01:26 +0800

    1

--- erp_project/src/main/java/com/ssm/service/device/DeviceService.java
+++ erp_project/src/main/java/com/ssm/service/device/DeviceService.java
@@ -3,8 +3,12 @@ package com.ssm.service.device;
 
 
 
+import com.ssm.bean.device.DevicePage;
+import com.ssm.bean.device.DeviceType;
+
 import java.util.List;
 
 public interface DeviceService {
-    //List<DeviceType> queryDeviceType(DevicePage devicePage);
+    List<DeviceType> queryDeviceType(DevicePage devicePage);
+    int queryAllDeviceTypeRecord();
 }
--- erp_project/src/main/java/com/ssm/service/device/impl/DeviceServiceImpl.java
+++ erp_project/src/main/java/com/ssm/service/device/impl/DeviceServiceImpl.java
@@ -1,13 +1,31 @@
 package com.ssm.service.device.impl;
 
 
+import com.ssm.bean.device.DevicePage;
+import com.ssm.bean.device.DeviceType;
+import com.ssm.bean.device.DeviceTypeExample;
+import com.ssm.mapper.device.DeviceTypeMapper;
 import com.ssm.service.device.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Service
 public class DeviceServiceImpl implements DeviceService {
-/*    @Override
+
+    @Autowired
+    DeviceTypeMapper deviceTypeMapper;
+    @Override
     public List<DeviceType> queryDeviceType(DevicePage devicePage) {
+
         return null;
-    }*/
+    }
+
+    @Override
+    public int queryAllDeviceTypeRecord() {
+        DeviceTypeExample deviceTypeExample = new DeviceTypeExample();
+        return (int) deviceTypeMapper.countByExample(deviceTypeExample);
+    }
 }
