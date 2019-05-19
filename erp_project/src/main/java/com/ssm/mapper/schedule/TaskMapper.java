6f93a317a3b2eb6d789b6adbcccffc5f6cb3249b
wanwufusu <760931928@qq.com>
2019-05-19 23:45:02 +0800

    邹睿明：修改了work get_data 的bug

---
+++ erp_project/src/main/java/com/ssm/mapper/schedule/TaskMapper.java
@@ -0,0 +1,17 @@
+package com.ssm.mapper.schedule;
+
+import com.ssm.bean.schedule.Task;
+
+public interface TaskMapper {
+    int deleteByPrimaryKey(String taskId);
+
+    int insert(Task record);
+
+    int insertSelective(Task record);
+
+    Task selectByPrimaryKey(String taskId);
+
+    int updateByPrimaryKeySelective(Task record);
+
+    int updateByPrimaryKey(Task record);
+}
